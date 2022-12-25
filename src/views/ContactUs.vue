e50991722840e2ab275b4c5d3a7bfcb97274e974
MRSN37 <raflysuryanirwana@gmail.com>
2022-12-25 21:36:35 +0700

    UPDATE: ContactUs and SignIn Page

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,34 +1,42 @@
 <template>
   <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    <ion-page>
-      <ion-content :fullscreen="true">
-        <ion-row>
-          <ion-toolbar style="color:#343A40">
-        <ion-title style="color:white; margin-left :20px; font-size: 25px;">Kampung<span style="color:#00DC72; font-size: 25px;">Siber</span></ion-title>
-        <ion-buttons class ="button-nav" slot="primary" style="color: white;" >
-          <ion-button class="ion-text-capitalize" style="font-size: 20px; margin: 20px; margin-left: 30px;" href="/LandingPage">
-            Beranda
-          </ion-button>
-          <ion-button class="ion-text-capitalize" style="font-size: 20px; margin: 20px; margin-left: 30px;" href="/FaqPage">
-            FAQ
-          </ion-button>
-          <ion-button class="ion-text-capitalize" style="font-size: 20px; margin: 20px; margin-left: 30px;" href="/LandingPage">
-            Contact Us
-          </ion-button>
-          <ion-button class="ion-text-capitalize" style="font-size: 20px; margin: 20px; margin-left: 30px;" href="/SignIn">
-            Sign In
-          </ion-button>
-        </ion-buttons>
-      </ion-toolbar>
-    </ion-row>
-        <ion-grid>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-row>
+        <ion-toolbar style="color:#343A40">
+          <ion-title style="color:white; margin-left :20px; font-size: 25px;">Kampung<span
+              style="color:#00DC72; font-size: 25px;">Siber</span></ion-title>
+          <ion-buttons class="button-nav" slot="primary" style="color: white;">
+            <ion-button class="ion-text-capitalize" style="font-size: 20px; margin: 20px; margin-left: 30px;"
+              href="/LandingPage">
+              Beranda
+            </ion-button>
+            <ion-button class="ion-text-capitalize" style="font-size: 20px; margin: 20px; margin-left: 30px;"
+              href="/FaqPage">
+              FAQ
+            </ion-button>
+            <ion-button class="ion-text-capitalize" style="font-size: 20px; margin: 20px; margin-left: 30px;"
+              href="/LandingPage">
+              Contact Us
+            </ion-button>
+            <ion-button class="ion-text-capitalize" style="font-size: 20px; margin: 20px; margin-left: 30px;"
+              href="/SignIn">
+              Sign In
+            </ion-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-row>
+      <ion-grid>
         <ion-row>
           <ion-col>
-          <div class="ion-text-start" style="color:black">
-            <h1 style="color:black; font-size: 65px; font-weight: bold; text-align: start; margin-left: 100px; margin-top: 40px;">Contact Us</h1>
-            <h2 style="color:black; font-size: 30px; font-weight: bold; text-align: start; margin-left: 100px;">Bingung dengan fitur, atau dengan konsultasi yang kami berikan
-            bisa langsung mengontak kami</h2>
-          </div>
+            <div class="ion-text-start" style="color:black">
+              <h1
+                style="color:black; font-size: 65px; font-weight: bold; text-align: start; margin-left: 100px; margin-top: 40px;">
+                Contact Us</h1>
+              <h2 style="color:black; font-size: 30px; font-weight: bold; text-align: start; margin-left: 100px;">
+                Bingung dengan fitur, atau dengan konsultasi yang kami berikan
+                bisa langsung mengontak kami</h2>
+            </div>
           </ion-col>
           <ion-col>
             <div class="ion-text-start" style="color:black">
@@ -82,82 +90,91 @@
     </ion-page>
   </template>
   
-  <script lang="ts">
-  import { IonContent, IonPage,IonToolbar, IonTitle,IonGrid,IonCol,IonRow,IonButtons,IonButton} from '@ionic/vue';
-  import { defineComponent } from 'vue';
-  
-  
-  export default defineComponent({
-    name: 'ContactUs',
-    components: {
-      IonContent,
-      IonPage,IonToolbar, IonTitle,IonGrid,IonCol,IonRow,IonButtons,IonButton,
-    }
-  });
-  </script>
-  <style>
-  * {
-    margin: 0;
-    padding: 0;
-    font-family: "Poppins";
+<script lang="ts">
+import { IonContent, IonPage, IonToolbar, IonTitle, IonGrid, IonCol, IonRow, IonButtons, IonButton } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'ContactUs',
+  components: {
+    IonContent,
+    IonPage, IonToolbar, IonTitle, IonGrid, IonCol, IonRow, IonButtons, IonButton,
+  }
+});
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Poppins";
 }
+
 #container {
-    text-align: center;
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 50%;
-    transform: translateY(-50%);
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
 }
+
 #container strong {
-    font-size: 20px;
-    line-height: 26px;
+  font-size: 20px;
+  line-height: 26px;
 }
+
 #container p {
-    font-size: 16px;
-    line-height: 22px;
-    color: #8c8c8c;
-    margin: 0;
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
 }
+
 #container a {
-    text-decoration: none;
+  text-decoration: none;
 }
+
 .card {
-    margin-left: 100px;
-    margin-top: 30px;
-    width: 900px;
-    align-items: left;
-    border-radius: 40px;
-    margin-bottom: 50px;
+  margin-left: 100px;
+  margin-top: 30px;
+  width: 900px;
+  align-items: left;
+  border-radius: 40px;
+  margin-bottom: 50px;
 }
+
 .title {
-    font-weight: 600;
-    font-size: 40px;
-    line-height: 60px;
-    text-align: center;
-    color: black;
-    padding-top: 20px;
+  font-weight: 600;
+  font-size: 40px;
+  line-height: 60px;
+  text-align: center;
+  color: black;
+  padding-top: 20px;
 }
+
 .text {
-    font-size: 40px;
-    padding-top: 15px;
-    line-height: 52px;
-    color: black;
-    padding-left: 41px;
+  font-size: 40px;
+  padding-top: 15px;
+  line-height: 52px;
+  color: black;
+  padding-left: 41px;
 }
-.question{
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 52px;
-    line-height: 110px;
-    margin-left: 20px;
+
+.question {
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 52px;
+  line-height: 110px;
+  margin-left: 20px;
 }
+
 .textbox {
-    margin: 0px 30px;
-    width: auto;
-    height: 100px;
-    background: linear-gradient(0deg, #EFEFEF, #EFEFEF), linear-gradient(0deg, #EFEFEF, #EFEFEF), #EFEFEF;
-    border-radius: 30px;
+  margin: 0px 30px;
+  width: auto;
+  height: 100px;
+  background: linear-gradient(0deg, #EFEFEF, #EFEFEF), linear-gradient(0deg, #EFEFEF, #EFEFEF), #EFEFEF;
+  border-radius: 30px;
 }
 .textbox1 {
     background: #FFFFFF;
@@ -171,32 +188,38 @@
     padding: 0px 15px;
 }
 .textbox2 {
-    margin: 0px 30px;
-    width: 1000px;
-    height: 400px;
-    background: linear-gradient(0deg, #EFEFEF, #EFEFEF), linear-gradient(0deg, #EFEFEF, #EFEFEF), #EFEFEF;
-    border-radius: 30px;
+  margin: 0px 30px;
+  width: 1000px;
+  height: 400px;
+  background: linear-gradient(0deg, #EFEFEF, #EFEFEF), linear-gradient(0deg, #EFEFEF, #EFEFEF), #EFEFEF;
+  border-radius: 30px;
 }
+
 .text-box {
-    font-weight: 500;
-    font-size: 30px;
-    line-height: 80px;
-    margin-left: 20px;
+  font-weight: 500;
+  font-size: 30px;
+  line-height: 80px;
+  color: black;
+
+  margin-left: 20px;
 }
+
 .btn1 {
-    --background: #ffffff;
-    --color: white;
-    --border-radius: 20px;
-    margin-top: 40px;
-    margin-left: 30px;
-    text-transform: none;
-    width: 250px;
-    height: 100px;
+  --background: #ffffff;
+  --color: white;
+  --border-radius: 20px;
+  margin-top: 40px;
+  margin-left: 30px;
+  text-transform: none;
+  width: 250px;
+  height: 100px;
 }
+
 .btn1text {
-    font-size: 35px;
-    line-height: 60px;
+  font-size: 35px;
+  line-height: 60px;
 }
+
 #footer1,
 #footer2 {
   font-family: 'Poppins';
@@ -204,11 +227,24 @@
   font-size: 25px;
   padding: 56px 21px;
 }
+
+.textbox1 {
+    background: #EFEFEF;
+    border: 1px solid #000000;
+    border-radius: 30px;
+    width: 100%;
+    height: 350px;
+    margin: 0px 30px;
+    color: black;
+    font-size: 35px;
+    padding: 0px 15px 20px 15px
+}
+
 #footer1 {
   text-align: left;
 }
+
 #footer2 {
   text-align: right;
 }
-
 </style>
